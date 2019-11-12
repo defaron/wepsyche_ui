ca73aca7bea8afec60e73247701c80b9ed59fe17
Edgar Jimenez <EJimenez@mtpsites.com>
2019-11-12 17:30:40 -0600

    First project version

---
+++ src/app/home-carousel/home-carousel.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-home-carousel',
+  templateUrl: './home-carousel.component.html',
+  styleUrls: ['./home-carousel.component.scss']
+})
+export class HomeCarouselComponent implements OnInit {
+
+  constructor(config: NgbCarouselConfig) {
+    config.interval = 2000;
+    config.wrap = true;
+    config.keyboard = false;
+    config.pauseOnHover = false;
+  }
+
+  ngOnInit() {
+  }
+
+}
